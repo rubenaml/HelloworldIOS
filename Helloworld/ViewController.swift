876eeeba55c483480428a70f30f7dee6a39b01ec
Ricardo <ricardo@ricardos-mac.local>
2019-03-02 08:40:08 -0800

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Ricardo on 3/2/19.
+//  Copyright © 2019 Tecguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
